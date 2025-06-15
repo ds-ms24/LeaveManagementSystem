7e8bbfa5365258de6a356175c79b5227c7159f68
Daniel Ovens <ds94.ms24@gmail.com>
2025-06-15 19:55:15 +0800

    postgres connected migration created

--- LeaveManagementSystem.Web/Data/ApplicationDbContext.cs
+++ LeaveManagementSystem.Web/Data/ApplicationDbContext.cs
@@ -5,9 +5,18 @@ namespace LeaveManagementSystem.Web.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
+        //protected readonly IConfiguration Configuration;
+
+        //public ApplicationDbContext(IConfiguration configuration)
+        //{
+        //    Configuration = configuration;
+        //}
+
     }
 }
