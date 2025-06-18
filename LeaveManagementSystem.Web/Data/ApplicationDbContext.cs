d286288cb4f04468718835e91a841c2d1dd22277
Daniel Ovens <ds94.ms24@gmail.com>
2025-06-18 20:08:18 +0800

    noted constructor in DbContext added leavetype and migrated it add and updated database

--- LeaveManagementSystem.Web/Data/ApplicationDbContext.cs
+++ LeaveManagementSystem.Web/Data/ApplicationDbContext.cs
@@ -3,12 +3,14 @@ using Microsoft.EntityFrameworkCore;
 
 namespace LeaveManagementSystem.Web.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext // this defines the class
     {
-        
+        // this is the constructor:
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }   
+
+        public DbSet<LeaveType> LeaveTypes { get; set; }
     }
 }
